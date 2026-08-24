a1b46a511b2be37fb9dba5a84dad83a831ad3ca6
bymajevich-ops <bymajevich@gmail.com>
2026-08-25 00:49:02 +0700

    Обновить кэш PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,17 @@
-const cacheName = "focus-v12";
+const cacheName = "focus-v13";
 const assets = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg", "./assets-background.png"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets))));
-self.addEventListener("fetch", (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))));
+self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response.ok && new URL(event.request.url).origin === location.origin) {
+          caches.open(cacheName).then((cache) => cache.put(event.request, response.clone()));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || (event.request.mode === "navigate" ? caches.match("./") : undefined)))
+  );
+});
