3cff08bfe692e96f02cbaa9148945b25260381ad
bymajevich-ops <bymajevich@gmail.com>
2026-08-25 00:55:08 +0700

    Добавить favicon и настройки напоминаний

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const cacheName = "focus-v13";
-const assets = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg", "./assets-background.png"];
+const cacheName = "focus-v14";
+const assets = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./favicon.png", "./assets-background.png"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets))));
 self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
 self.addEventListener("fetch", (event) => {
